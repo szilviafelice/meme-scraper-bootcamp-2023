5bee77d2efa7a742170a5d4dd59a8f54b9e8970e
szilviafelice <szilvia.felice@gmail.com>
2023-09-07 13:00:05 -0400

    Downloading img is solved

--- web-scraper-nodejs/index.js
+++ web-scraper-nodejs/index.js
@@ -1,10 +1,10 @@
 import fs from 'node:fs';
+import https from 'node:https';
+import path from 'node:path';
 import axios from 'axios';
 import { JSDOM } from 'jsdom';
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
-
-const options = { hostname: 'memegen-link-examples-upleveled.netlify.app' };
 
 const folderName = 'memes';
 try {
@@ -13,17 +13,34 @@ try {
   }
   axios
     .get(url)
-    .then((kiscica) => {
-      const dom = new JSDOM(kiscica.data);
-      const elements = dom.window.document.querySelectorAll('img');
-      const imgElements = Array.from(elements);
-      const slicedArray = imgElements.slice(0, 10);
-      const urls = [];
-      for (const image of slicedArray) {
-        urls.push(image.src);
+    .then((websiteData) => {
+      const dom = new JSDOM(
+        websiteData.data,
+      ); /* making dom from raw website data */
+      const elements =
+        dom.window.document.querySelectorAll(
+          'img',
+        ); /* getting all img tags from dom */
+      const imgElements =
+        Array.from(elements); /* from img elements make an array */
+      const slicedArray = imgElements.slice(
+        0,
+        10,
+      ); /* get the first 10 elements */
+      for (let i = 0; i < slicedArray.length; i++) {
+        /* starting from 0 until 1 less than arrays length, iterate */
+        const filename = `${(i + 1)
+          .toString()
+          .padStart(
+            2,
+            '0',
+          )}.jpg`; /* in each loop add index + 1 and pad with 0 */
+        const filePath = path.join(folderName, filename); /* make filepath */
+        const imgFile = fs.createWriteStream(filePath); /* create file */
+        https.get(slicedArray[i].src, (response) =>
+          response.pipe(imgFile),
+        ); /* feed downloaded img to open file*/
       }
-
-      console.log(urls);
     })
     .catch((error) => console.log(error));
 } catch (err) {
